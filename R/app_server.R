72371101e5b362c51a35b32ce4f7131100ac36f5
Khaled Al-Shamaa <khaled.alshamaa@gmail.com>
2023-11-03 15:06:35 +0300

    Include all required columns to mapping

--- R/app_server.R
+++ R/app_server.R
@@ -6,8 +6,11 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+  required_mapping <- c("stage", "pipeline", "country", "year", "season",
+                        "location", "trial", "environment", "rep", "iBlock",
+                        "row", "col", "designation", "gid", "entryType", "trait")
 
-  data.sta <- mod_getData_server("getData_1", map = c("trait", "designation"))
+  data.sta <- mod_getData_server("getData_1", map = required_mapping)
   #data.sta <- mod_input_file_sta_server("input_file_sta_1")
   mod_qaRawApp_server("qaRawApp_1", data.sta)
   mod_staApp_server("staApp_1", data.sta)
