4aeab27449c3da06926925842a317476fe732d50
covaruber <cova_ruber@live.com.mx>
2023-11-03 13:49:42 +0300

    qa module

--- R/mod_qaRawApp.R
+++ R/mod_qaRawApp.R
@@ -11,7 +11,8 @@ mod_qaRawApp_ui <- function(id){
   ns <- NS(id)
   tagList(
 
-    sidebarPanel(
+    shiny::sidebarPanel(#width = 3,
+      width = 3,
       tags$style(".well {background-color:grey; color: #FFFFFF;}"),
       div(tags$p( "Outlier detection")),#, style = "color: #817e7e"
       hr(style = "border-top: 1px solid #4c4c4c;"),
@@ -25,33 +26,20 @@ mod_qaRawApp_ui <- function(id){
                           numericInput(ns("outlierCoefOutqFont"), label = "x-axis font size", value = 12, step=1)
       ),
       hr(style = "border-top: 1px solid #4c4c4c;"),
-      # actionButton(ns("runOutqPheno"), "Run", icon = icon("play-circle")),
+      actionButton(ns("runQaRaw"), "Run", icon = icon("play-circle")),
       hr(style = "border-top: 1px solid #4c4c4c;"),
-      # shinycssloaders::withSpinner(textOutput(ns("outQaRaw")),type=8)
+      shinycssloaders::withSpinner(textOutput(ns("outQaRaw")),type=8)
     ), # end sidebarpanel
-    mainPanel(tabsetPanel(
+    shiny::mainPanel(width = 9,
+      tabsetPanel( #width=9,
       type = "tabs",
 
       tabPanel("Outlier detection",
                br(),
                shinydashboard::box(status="primary",width = 12,
                                    solidHeader = TRUE,
-                                   plotly::plotlyOutput(ns("plotPredictionsCleanOut"))
-                                   # column(width=12,DT::DTOutput(ns("phenoQaRaw")),style = "height:800px; overflow-y: scroll;overflow-x: scroll;")
-               )
-      ),
-      tabPanel("Modifications",
-               br(),
-               shinydashboard::box(status="primary",width = 12,
-                                   solidHeader = TRUE,
+                                   plotly::plotlyOutput(ns("plotPredictionsCleanOut")),
                                    column(width=12,DT::DTOutput(ns("modificationsQa")),style = "height:800px; overflow-y: scroll;overflow-x: scroll;")
-               )
-      ),
-      tabPanel("Data",
-               br(),
-               shinydashboard::box(status="primary",width = 12,
-                                   solidHeader = TRUE,
-                                   column(width=12,DT::DTOutput(ns("phenoQaRaw")),style = "height:800px; overflow-y: scroll;overflow-x: scroll;")
                )
       ),
       tabPanel("Documentation",
@@ -103,7 +91,7 @@ mod_qaRawApp_server <- function(id, data){
       mydata$rowindex <- 1:nrow(mydata)
       mydata[, "environment"] <- as.factor(mydata[, "environment"])
       traitClasses <- unlist(lapply(mydata, class))
-      analysisId <- as.numeric(Sys.time())
+      analysisId <- NA#as.numeric(Sys.time())
       myoutliers <- myObject$modifications$pheno
       if(!is.null(myoutliers) & !is.null(nrow(myoutliers)) ){ # there's previous outliers for this trait
         outsItrait <- which(myoutliers$trait == input$traitOutqPheno)
@@ -119,13 +107,13 @@ mod_qaRawApp_server <- function(id, data){
         if(!is.na(input$outlierCoefOutqPheno)){
           outlier <- grDevices::boxplot.stats(x=sampleDT[, input$traitOutqPheno],coef=input$outlierCoefOutqPheno )$out
           toSilence <- sampleDT[which(sampleDT[,input$traitOutqPheno] %in% outlier),"rowindex"]
-          typeOut <- rep("outlier",length(toSilence))
+          typeOut <- rep("outlierIQR",length(toSilence))
         }else{
           toSilence <- numeric()
           typeOut <- character()
         }
         outOfBounds <- which((sampleDT[, input$traitOutqPheno] < input$traitLBOutqPheno) | (sampleDT[, input$traitOutqPheno] > input$traitUBOutqPheno ) )
-        if(length(outOfBounds) > 0){toSilence <- c(toSilence, sampleDT[outOfBounds,"rowindex"]); typeOut <- c(typeOut, rep("outlier",length(outOfBounds))) }
+        if(length(outOfBounds) > 0){toSilence <- c(toSilence, sampleDT[outOfBounds,"rowindex"]); typeOut <- c(typeOut, rep("outlierIQR",length(outOfBounds))) }
         if(length(toSilence) > 0){
           outList[[counter]] <- data.frame(module="qaRaw",analysisId=analysisId,trait=input$traitOutqPheno,reason=typeOut,row=toSilence, value=NA);
           counter=counter+1
@@ -177,140 +165,62 @@ mod_qaRawApp_server <- function(id, data){
       res
     })
 
-
-
-    ## display pheno data
+    ## display the current outliers
     observeEvent(data(),{
 
-      output$phenoQaRaw <-  DT::renderDT({
-        # if ( hideAll$clearAll)
-        #   return()
-        # else
-          req(data())
-          dtQaRaw <- data()
-          dtQaRaw <- dtQaRaw$data$pheno
-          DT::datatable(dtQaRaw,
-                        options = list(autoWidth = TRUE),
-                        filter = "top"
-          )
+      output$modificationsQa <-  DT::renderDT({
+
+        req(data())
+        req(input$outlierCoefOutqFont)
+        req(input$traitOutqPheno)
+        ## get the outlier table
+        outlier <- newOutliers()
+        removeCols <- c("module","analysisId","value")
+        outlier <- outlier[, setdiff(colnames(outlier),removeCols)]
+        colnames(outlier) <- cgiarBase::replaceValues(Source = colnames(outlier), Search = "row", Replace = "record")
+        ## add phenotypic data
+        dtQaRaw <- data()
+        dtQaRaw <- dtQaRaw$data$pheno
+        dtQaRaw$outlierRow <- 1:nrow(dtQaRaw)
+        removeCols <- c("stage","pipeline","country","year","season","location","trial","gid")
+        dtQaRaw <- dtQaRaw[,unique(c("outlierRow",setdiff(colnames(dtQaRaw), removeCols)))]
+        ## merge
+        myTable <- base::merge(outlier,dtQaRaw, by.x="record", by.y="outlierRow", all.x=TRUE)
+
+        DT::datatable(myTable,
+                      options = list(autoWidth = TRUE),
+                      filter = "top"
+        )
 
       })
 
     })
 
-    # ## render result of "run" button click
-    # outQaRaw <- eventReactive(input$runQaRaw, {
-    #   req(data())
-    #   req(input$trait2QaRaw)
-    #   req(input$genoUnitQaRaw)
-    #   dtQaRaw <- data()
-    #
-    #   myFamily = apply(xx$df,2,function(y){rownames(xx$df)[which(y > 0)[1]]})
-    #   dontHaveDist <- which(is.na(myFamily))
-    #   if(length(dontHaveDist) > 0){myFamily[dontHaveDist] <- "gaussian(link = 'identity')"}
-    #
-    #   # run the modeling, but before test if qa/qc done
-    #   if(sum(dtQaRaw$status$module %in% "qa") == 0) {
-    #     output$qaQcQaRawInfo <- renderUI({
-    #       if (hideAll$clearAll)
-    #         return()
-    #       else
-    #         req(dtQaRaw)
-    #       HTML(as.character(div(style="color: brown;",
-    #                             "Please perform QA/QC before conducting a Single-Trial Analysis."))
-    #       )
-    #     })
-    #   } else {
-    #     output$qaQcQaRawInfo <- renderUI({return(NULL)})
-    #     result <- try(cgiarPipe::staLMM(phenoDTfile = dtQaRaw,
-    #                                     trait=input$trait2QaRaw, traitFamily = myFamily,
-    #                                     fixedTerm = input$fixedTermQaRaw2,
-    #                                     returnFixedGeno=input$genoAsFixedQaRaw, genoUnit = input$genoUnitQaRaw,
-    #                                     verbose = input$verboseQaRaw, maxit = input$maxitQaRaw),
-    #                   silent=TRUE
-    #     )
-    #   }
-    #
-    #   if(sum(dtQaRaw$status$module %in% "qa") != 0) {
-    #
-    #     output$predictionsQaRaw <-  DT::renderDT({
-    #       if(!inherits(result,"try-error") ){
-    #         if ( hideAll$clearAll)
-    #           return()
-    #         else
-    #           predictions <- result$predictions
-    #         predictions <- predictions[predictions$module=="sta",]
-    #
-    #         predictions$analysisId <- as.numeric(predictions$analysisId)
-    #         predictions <- predictions[!is.na(predictions$analysisId),]
-    #
-    #
-    #         current.predictions <- predictions[predictions$analysisId==max(predictions$analysisId),]
-    #         current.predictions <- subset(current.predictions, select = -c(module,analysisId))
-    #         numeric.output <- c("predictedValue", "stdError", "reliability")
-    #         DT::formatRound(DT::datatable(current.predictions,
-    #                                       options = list(autoWidth = TRUE),
-    #                                       filter = "top"
-    #         ), numeric.output)
-    #       }
-    #     })
-    #
-    #     output$metricsQaRaw <-  DT::renderDT({
-    #       if(!inherits(result,"try-error") ){
-    #         if ( hideAll$clearAll)
-    #           return()
-    #         else
-    #           metrics <- result$metrics
-    #         metrics <- metrics[metrics$module=="sta",]
-    #         metrics$analysisId <- as.numeric(metrics$analysisId)
-    #         metrics <- metrics[!is.na(metrics$analysisId),]
-    #         current.metrics <- metrics[metrics$analysisId==max(metrics$analysisId),]
-    #         current.metrics <- subset(current.metrics, select = -c(module,analysisId))
-    #         numeric.output <- c("value", "stdError")
-    #         DT::formatRound(DT::datatable(current.metrics,
-    #                                       options = list(autoWidth = TRUE),
-    #                                       filter = "top"
-    #         ), numeric.output)
-    #       }
-    #     })
-    #
-    #     output$modelingQaRaw <-  DT::renderDT({
-    #       if(!inherits(result,"try-error") ){
-    #         if ( hideAll$clearAll)
-    #           return()
-    #         else
-    #           modeling <- result$modeling
-    #         modeling <- modeling[modeling$module=="sta",]
-    #
-    #         modeling$analysisId <- as.numeric(modeling$analysisId)
-    #         modeling <- modeling[!is.na(modeling$analysisId),]
-    #
-    #         current.modeling <- modeling[modeling$analysisId==max(modeling$analysisId),]
-    #         current.modeling <- subset(current.modeling, select = -c(module,analysisId))
-    #         DT::datatable(current.modeling,
-    #                       options = list(autoWidth = TRUE),
-    #                       filter = "top"
-    #         )
-    #       }
-    #     })
-    #   } else {
-    #     output$predictionsQaRaw <- DT::renderDT({DT::datatable(NULL)})
-    #     output$metricsQaRaw <- DT::renderDT({DT::datatable(NULL)})
-    #     output$modelingQaRaw <- DT::renderDT({DT::datatable(NULL)})
-    #   }
-    #
-    #   hideAll$clearAll <- FALSE
-    #
-    # }) ## end eventReactive
-    #
-    # output$outQaRaw <- renderPrint({
-    #   outQaRaw()
-    # })
+    ## save when user clicks
 
-    # output$markdown <- renderUI({
-    #   HTML(markdown::markdownToHTML(knitr::knit('R/testing_sta.Rmd', quiet = TRUE)))
-    # })
+    outQaRaw <- eventReactive(input$runQaRaw, {
 
+      req(data())
+      req(input$outlierCoefOutqFont)
+      req(input$traitOutqPheno)
+      ## get the outlier table
+      outlier <- newOutliers()
+      ## get data structure
+      dtQaRaw <- data()
+      ## update analsisId in the outliers table
+      analysisId <- as.numeric(Sys.time())
+      outlier[which(is.na(outlier$analysisId)),"analysisId"] <- analysisId
+      ## bind new parameters
+      dtQaRaw$modifications <- rbind(dtQaRaw$modifications, outlier[, colnames(dtQaRaw$modifications)])
+      dtQaRaw$status <- rbind(dtQaRaw$status, data.frame(module="qaRaw", analysisId=analysisId ))
+      fileId <- dtQaRaw$metadata$general[which(dtQaRaw$metadata$general$parameter == "fileId"),"value"]
+      save(dtQaRaw, file=paste0(fileId,".RData"))
+
+
+    })
+    output$outQaRaw <- renderPrint({
+      outQaRaw()
+    })
 
 
   })
