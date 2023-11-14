549e7b3a51f58714cdcc759af49802c72aba35ce
covaruber <cova_ruber@live.com.mx>
2023-11-14 15:21:17 +0800

    new menu

--- R/app_ui.R
+++ R/app_ui.R
@@ -23,23 +23,15 @@ app_ui <- function(request) {
       # tabPanel("Data Input ", mod_input_file_sta_ui("input_file_sta_1")),
 
       navbarMenu("Genetic Evaluation",
-                 tabPanel("QA/QC (raw-data)", mod_qaRawApp_ui("qaRawApp_1") ),
-                 tabPanel("Single-Trial Analysis",  mod_staApp_ui("staApp_1")),
-                 tabPanel("QA/QC (model-based)", mod_qaStaApp_ui("qaStaApp_1") ) ,
-                 tabPanel("Marker QA/QC (optional)",  ),
-                 tabPanel("Multi-Trial Analysis", mod_mtaApp_ui("mtaApp_1") ), # biplot is part of the report in MET
-                 tabPanel("Selection Indices", mod_indexDesireApp_ui("indexDesireApp_1") ) ,
-                 tabPanel("Optimal Cross Selection", mod_ocsApp_ui("ocsApp_1") )
-      ),
-
-      navbarMenu("Genetic Gain",
-                 tabPanel("QA/QC (raw data)", ),
-                 tabPanel("Single-Trial Analysis",  ),
-                 tabPanel("QA/QC (model-based)",  ) ,
-                 tabPanel("Marker QA/QC (optional)",  ),
-                 tabPanel("Multi-Trial Analysis",  ), # biplot is part of the report in MET
-                 tabPanel("Realized Genetic Gain", ), # user needs to do up to a multi-year genetic evaluation to provide the MET as input
-                 tabPanel("Predicted Genetic Gain", )# user needs to perform a multi-year genetic evaluation to provide the MET as input
+                 tabPanel("1. QA/QC (raw-data)", mod_qaRawApp_ui("qaRawApp_1") ),
+                 tabPanel("2. Single-Trial Analysis",  mod_staApp_ui("staApp_1")),
+                 tabPanel("3. QA/QC (model-based)", mod_qaStaApp_ui("qaStaApp_1") ) ,
+                 tabPanel("* Marker QA/QC (optional)",  ),
+                 tabPanel("4. Multi-Trial Analysis", mod_mtaApp_ui("mtaApp_1") ), # biplot is part of the report in MET
+                 tabPanel("5. Selection Indices", mod_indexDesireApp_ui("indexDesireApp_1") ) ,
+                 tabPanel("6. Optimal Cross Selection", mod_ocsApp_ui("ocsApp_1") ),
+                 tabPanel("7.1. Realized Genetic Gain", ), # user needs to do up to a multi-year genetic evaluation to provide the MET as input
+                 tabPanel("7.2. Predicted Genetic Gain", )# user needs to perform a multi-year genetic evaluation to provide the MET as input
       ),
 
       navbarMenu("QTL Mapping",
@@ -50,17 +42,17 @@ app_ui <- function(request) {
                  tabPanel("GWAS",  ) # user needs to make all the process up to Marker QA/QC
       ),
 
-      navbarMenu("Marker Structure ",
-                 tabPanel("Marker QA/QC",  ),
-                 tabPanel("Relationship Matrix (optional)",  ),
-                 tabPanel("Clustering", ) # using PCA, dendograms, etc. and as input either the markers or the relationship matrix
-      ),
-
-      navbarMenu("Trait Structure",
-                 tabPanel("Phenotype (QA/QC)", ),
-                 tabPanel("Phenotype (Clustering)", ) # using PCA, dendograms, etc. and as input either the phenotypes or MET results
-
-      ),
+      # navbarMenu("Marker Structure ",
+      #            tabPanel("Marker QA/QC",  ),
+      #            tabPanel("Relationship Matrix (optional)",  ),
+      #            tabPanel("Clustering", ) # using PCA, dendograms, etc. and as input either the markers or the relationship matrix
+      # ),
+      #
+      # navbarMenu("Trait Structure",
+      #            tabPanel("Phenotype (QA/QC)", ),
+      #            tabPanel("Phenotype (Clustering)", ) # using PCA, dendograms, etc. and as input either the phenotypes or MET results
+      #
+      # ),
 
 
 
