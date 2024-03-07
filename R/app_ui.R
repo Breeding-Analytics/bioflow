d9380d51d1e6864263424b2dc8a77255bad62ff9
Ibnou Dieng <i.dieng@cgiar.org>
2024-03-07 11:13:43 +0100

    Reorganize QA/QC and STA menus

--- R/app_ui.R
+++ R/app_ui.R
@@ -37,12 +37,36 @@ app_ui <- function(request) {
 
       tabPanel("Data Retrieval ", mod_getData_ui("getData_1"), icon = icon("upload") ),
 
+      # navbarMenu("Selection", icon = icon("bullseye"),
+      #            tabPanel(div(icon("filter-circle-xmark"), "Pheno-Outlier QA/QC (", icon("seedling"),")" ), mod_qaRawApp_ui("qaRawApp_1") ),
+      #            tabPanel(strong("GENETIC EVALUATION"), mod_sectionInfoGEApp_ui("sectionInfoGEApp_1") ),
+      #            tabPanel(div(icon("calculator"), icon("dice-one"), "Single-Trial Analysis (", icon("seedling"), ")"),  mod_staApp_ui("staApp_1") ),
+      #            tabPanel(div(icon("filter-circle-xmark"), "(optional) Model-Based QA/QC (", icon("seedling"), ")" ), mod_qaStaApp_ui("qaStaApp_1") ) ,
+      #            tabPanel(div(icon("filter-circle-xmark"), "(optional) Genetic-Marker QA/QC (", icon("dna"), ")" ), mod_qaGenoApp_ui("qaGenoApp_1")  ),
+      #            tabPanel(div(icon("calculator"), icon("dice-two"), "Multi-Trial Analysis (", icon("seedling"), icon("dna"), icon("network-wired"), icon("cloud-bolt") ,")"), mod_mtaApp_ui("mtaApp_1") ), # biplot is part of the report in MET
+      #            tabPanel(div(icon("calculator"), icon("dice-three"), "Selection Indices (",icon("seedling"),")" ), mod_indexDesireApp_ui("indexDesireApp_1") ) ,
+      #            tabPanel(div(icon("calculator"), icon("dice-four"), "Optimal Cross Selection (", icon("seedling"), ")" ), mod_ocsApp_ui("ocsApp_1") ),
+      #            tabPanel(strong("SELECTION HISTORY"), mod_sectionInfoSHApp_ui("sectionInfoSHApp_1") ),  # chart-line , barcode
+      #            tabPanel(div(icon("chart-line"), "Realized Genetic Gain (", icon("seedling"), icon("network-wired"),")"), mod_rggApp_ui("rggApp_1") ), # user needs to do up to a multi-year genetic evaluation to provide the MET as input
+      #            tabPanel(div(icon("chart-line"), "Predicted Genetic Gain (", icon("seedling"),  icon("network-wired"),")"), mod_pggApp_ui("pggApp_1")),# user needs to perfor m a multi-year genetic evaluation to provide the MET as input
+      #            tabPanel(div(icon("chart-line"), "Selection signatures (", icon("dna"),")", style = "color:red") )#  , icon = icon("filter")) # may include P3D, traditional single linear regression, Eigen, etc.
+      # ),
+
       navbarMenu("Selection", icon = icon("bullseye"),
-                 tabPanel(div(icon("filter-circle-xmark"), "Pheno-Outlier QA/QC (", icon("seedling"),")" ), mod_qaRawApp_ui("qaRawApp_1") ),
                  tabPanel(strong("GENETIC EVALUATION"), mod_sectionInfoGEApp_ui("sectionInfoGEApp_1") ),
-                 tabPanel(div(icon("calculator"), icon("dice-one"), "Single-Trial Analysis (", icon("seedling"), ")"),  mod_staApp_ui("staApp_1") ),
-                 tabPanel(div(icon("filter-circle-xmark"), "(optional) Model-Based QA/QC (", icon("seedling"), ")" ), mod_qaStaApp_ui("qaStaApp_1") ) ,
-                 tabPanel(div(icon("filter-circle-xmark"), "(optional) Genetic-Marker QA/QC (", icon("dna"), ")" ), mod_qaGenoApp_ui("qaGenoApp_1")  ),
+                 tabPanel(div(icon("filter-circle-xmark"), "QA/QC"),
+                          tabsetPanel(
+                            tabPanel(div("Pheno-Outlier QA/QC (", icon("seedling"),")" ), mod_qaRawApp_ui("qaRawApp_1") ),
+                            tabPanel(div("(optional) Genetic-Marker QA/QC (", icon("dna"), ")" ), mod_qaGenoApp_ui("qaGenoApp_1") )
+                          )
+                 ),
+
+                 tabPanel(div(icon("calculator"), icon("dice-one"), "Single-Trial Analysis (", icon("seedling"), ")"),
+                          tabsetPanel(
+                            tabPanel(div("Single-Trial Analysis (", icon("seedling"), ")"),  mod_staApp_ui("staApp_1") ),
+                            tabPanel(div("(optional) Model-Based QA/QC (", icon("seedling"), ")" ), mod_qaStaApp_ui("qaStaApp_1") )
+                          )
+                 ),
                  tabPanel(div(icon("calculator"), icon("dice-two"), "Multi-Trial Analysis (", icon("seedling"), icon("dna"), icon("network-wired"), icon("cloud-bolt") ,")"), mod_mtaApp_ui("mtaApp_1") ), # biplot is part of the report in MET
                  tabPanel(div(icon("calculator"), icon("dice-three"), "Selection Indices (",icon("seedling"),")" ), mod_indexDesireApp_ui("indexDesireApp_1") ) ,
                  tabPanel(div(icon("calculator"), icon("dice-four"), "Optimal Cross Selection (", icon("seedling"), ")" ), mod_ocsApp_ui("ocsApp_1") ),
