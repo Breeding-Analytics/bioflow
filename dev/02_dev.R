24540b551910bdfae50ce6d0a485497962402567
serbier <serbier@users.noreply.github.com>
2024-06-05 09:52:39 -0500

    run Bioflow modifications

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -1,22 +1,10 @@
-# # Building a Prod-Ready, Robust Shiny Application.
-# #
-# # README: each step of the dev files is optional, and you don't have to
-# # fill every dev scripts before getting started.
-# # 01_start.R should be filled at start.
-# # 02_dev.R should be used to keep track of your development during the project.
-# # 03_deploy.R should be used once you need to deploy your app.
-# #
-# #
-# ###################################
-# #### CURRENT FILE: DEV SCRIPT #####
-# ###################################
+# Building a Prod-Ready, Robust Shiny Application.
 #
-# # Engineering
-#
-# ## Dependencies ----
-# ## Amend DESCRIPTION with dependencies read from package code parsing
-# ## install.packages('attachment') # if needed.
-# attachment::att_amend_desc()
+# README: each step of the dev files is optional, and you don't have to
+# fill every dev scripts before getting started.
+# 01_start.R should be filled at start.
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
 #
 #
 # usethis::use_package("ape")
