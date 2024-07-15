00d9867874047e8ff2924dc686c56d1b84024fe4
Khaled Al-Shamaa <khaled.alshamaa@gmail.com>
2024-07-15 13:11:17 +0300

    stop reading EBS token until fix it

--- R/mod_getDataPheno.R
+++ R/mod_getDataPheno.R
@@ -224,7 +224,6 @@ mod_getDataPheno_server <- function(id, map = NULL, data = NULL, res_auth=NULL){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
-
     # warning message
     output$warningMessage <- renderUI(
       if(is.null(data())){
@@ -365,9 +364,9 @@ mod_getDataPheno_server <- function(id, map = NULL, data = NULL, res_auth=NULL){
                                    client_secret = ebs_client_secret)
               } else {
                 # golem::invoke_js("getCookie")
-                ebs_token <- sub(".*ebs_token=([^;]*).*", "\\1", input$cookies)
+                # ebs_token <- sub(".*ebs_token=([^;]*).*", "\\1", input$cookies)
 
-                if (ebs_token == input$cookies) {
+                if (TRUE) {
                   shiny_app_uri <- paste0(session$clientData$url_protocol, '//',
                                           session$clientData$url_hostname,
                                           session$clientData$url_pathname)
