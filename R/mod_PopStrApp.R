11a252ed14b89719c48ececca0039f17d3e3235b
covaruber <cova_ruber@live.com.mx>
2024-07-30 22:12:40 +0000

    few changes to pop str module

--- R/mod_PopStrApp.R
+++ R/mod_PopStrApp.R
@@ -126,7 +126,7 @@ mod_PopStrApp_ui <- function(id){
                                                                                                             selectInput(ns('colordend'),'Choose a color',choices = '',selected="",multiple=T)
                                                                    ),
                                                                    div(plotOutput(ns("dend"),height = "750px",width = "1250px"),align="center")
-                                                                   
+
                                            ),
                                            shinydashboard::box(title="Factors and Groups", status="success",width = 12, solidHeader = TRUE,
                                                                column(width=12,DT::DTOutput(ns("seeDataMDS")),style = "overflow-y: scroll;overflow-x: scroll;")
@@ -229,12 +229,12 @@ mod_PopStrApp_server <- function(id, data){
         }
       }
    })
-    
+
     output$outPopStr <- renderUI({
       outPopStr()
     })
 
-    
+
     outPopStr <- eventReactive(input$runPopStr, {
           req(data())
           result <- data()
@@ -243,7 +243,7 @@ mod_PopStrApp_server <- function(id, data){
           dfenvbio <- NULL
           catv <-"GroupClust"
           if(input$geno_groupPopStr!=FALSE) {
-			        src <- normalizePath("inst/app/www/example/Groupgeno.csv")     
+			        src <- normalizePath("inst/app/www/example/Groupgeno.csv")
 			        dfenvbio <- read.csv(src)
 		      }
           if(length(input$fileenvbio$datapath)!=0) dfenvbio <- read.csv(input$fileenvbio$datapath)
@@ -253,19 +253,19 @@ mod_PopStrApp_server <- function(id, data){
           #uno <- Biodv(result,distk, nclust, dfenvbio, catv)
 		      uno <- cgiarBase::Biodv(result,distk, nclust, dfenvbio, catv)
           shinybusy::remove_modal_spinner()
-          
+
           rm(result)
       if(!inherits(uno,"try-error")) {
           result <- list(PopStr=uno[[4]])
           result$PopStr$AMOVA=uno[[7]]
           save(result,file=normalizePath("R/outputs/resultPopStr.RData"))
-          
+
           if(length(names(uno[[5]]))>5) catv<-names(uno[[5]])[6]
           txlab <- paste0('Factor 1 (',uno[[3]][1],'%)')
           tylab <- paste0('Factor 2 (',uno[[3]][2],'%)')
           tzlab <- paste0('Factor 3 (',uno[[3]][3],'%)')
           eti <- "Gen"
-          
+
           #Actualiza la variable catv (grupos) en conjunto con la seleccion de colores
           observeEvent(catv,{
             set.seed(7)
@@ -277,8 +277,8 @@ mod_PopStrApp_server <- function(id, data){
             updateSelectInput(session,'color','Choose a color',choices=d,selected=d[1:grupos])
             updateSelectInput(session,'colordend','Choose a color', choices=d,selected=d[1:grupos])
           })
-          
-      #For report  
+
+      #For report
       output$reportPopStr <- renderUI({
         HTML(markdown::markdownToHTML(knitr::knit(system.file("rmd","reportPopStr.Rmd",package="bioflow"), quiet = TRUE), fragment.only=TRUE))
       })
@@ -290,8 +290,12 @@ mod_PopStrApp_server <- function(id, data){
           ))
         },
         content = function(file) {
+          # src <- normalizePath(system.file("rmd","reportPopStr.Rmd",package="bioflow"))
+          # src2 <- normalizePath('./R/outputs/resultPopStr.RData')
+
           src <- normalizePath(system.file("rmd","reportPopStr.Rmd",package="bioflow"))
-          src2 <- normalizePath('./R/outputs/resultPopStr.RData')
+          src2 <- normalizePath('data/resultPopStr.RData')
+
           # temporarily switch to the temp dir, in case you do not have write
           # permission to the current working directory
           owd <- setwd(tempdir())
@@ -300,9 +304,11 @@ mod_PopStrApp_server <- function(id, data){
           file.copy(src2, 'resultPopStr.RData', overwrite = TRUE)
           out <- rmarkdown::render('report.Rmd', params = list(toDownload=TRUE),switch(
             "HTML",
-            HTML = rmarkdown::html_document()
+            # HTML = rmarkdown::html_document()
+            HTML = rmdformats::robobook(toc_depth = 4)
           ))
           file.rename(out, file)
+          shinybusy::remove_modal_spinner()
         }
       )
       #Ver datos en tabla dinamica Summary Diversity
@@ -316,11 +322,11 @@ mod_PopStrApp_server <- function(id, data){
                                        lengthMenu = list(c(10,20,50,-1), c(10,20,50,'All')))
           )
         })
-        
-       
+
+
         #Ver datos en tabla dinamica AMOVA
         output$seeDataGAMOVA<-DT::renderDataTable({
-          seedatosAV=as.data.frame(uno[[7]])  
+          seedatosAV=as.data.frame(uno[[7]])
           rownames(seedatosAV)=seedatosAV[,1]
           seedatosAV=seedatosAV[,-1]
           DT::datatable(seedatosAV, extensions = 'Buttons',
@@ -328,7 +334,7 @@ mod_PopStrApp_server <- function(id, data){
                                        lengthMenu = list(c(10,20,50,-1), c(10,20,50,'All')))
           )
         })
-        
+
         #Ver datos en tabla dinamica %NA, He, Ho by marker
         output$seeDataStatMark<-DT::renderDT({
           seedatosStaM<-as.data.frame(result[["PopStr"]][[4]])
@@ -365,7 +371,7 @@ mod_PopStrApp_server <- function(id, data){
           tylab2=txlab
           if(input$ycol=="Factor2") tylab2=tylab
           if(input$ycol=="Factor3") tylab2=tzlab
-          
+
           mydata<-data()$data$geno
           if(!is.null(mydata)){
             p=plotly::plot_ly(data=uno[[5]],x=uno[[5]][,input$xcol],y=uno[[5]][,input$ycol],color=uno[[5]][,catv],
@@ -383,7 +389,7 @@ mod_PopStrApp_server <- function(id, data){
             fig
           }
         })
-        
+
         #Plot heatmap
         output$heat=plotly::renderPlotly({
           distMat=result[["PopStr"]][["Distance_Matrix"]]
@@ -408,7 +414,7 @@ mod_PopStrApp_server <- function(id, data){
             info<- cbind(ID=info$Gen,info)
             names(info)=c("ID","Gen","Group")
             tree=uno[[6]]
-            
+
             if (input$typeclust=="rectangular"){
               plot(tree, type = "phylogram", cex = input$sizelab, label.offset = input$space, show.tip.label = TRUE, edge.color = "black", edge.width =input$sizeline, edge.lty = 1,tip.color = input$colordend[info$Group])
               legend(input$poslen, legend=levels(info$Group), fill=input$colordend,box.lty=0)
@@ -418,13 +424,13 @@ mod_PopStrApp_server <- function(id, data){
             }
           }
         })
-        
+
         if(length(input$fileenvbio$datapath)!=0 & catv=="GroupClust"){
           HTML( as.character(div(style="color:green ; font-size: 20px;", "Ready but please check the external groups file, did't find genotype names matches" )) )
         }else{
           HTML( as.character(div(style="color:green ; font-size: 20px;", "Ready" )) )
         }
-        
+
       }else{
         HTML( as.character(div(style="color:green ; font-size: 20px;", "Some errors were found" )) )
       }
---
+++ reportPopStr.md
@@ -0,0 +1,25 @@
+---
+title: "Population structure report"
+author: "Contact:<a href = 'https://github.com/Breeding-Analytics/bioflow' target = '_blank'>Breeding Analytics Team, OneCGIAR</a> breedinganalytics@cgiar.org"
+date: "July 30, 2024"  
+output: html_document
+params:
+toDownload: FALSE
+---
+  
+
+
+
+
+
+
+### Summary diversity
+
+The following table allows you to inspect the principal statistics for the diversity analysis.
+
+<p>&nbsp;</p>
+
+
+
+
+
